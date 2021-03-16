f6191214fd6de78c78e596aaac3d05a5635c8dc2
Steven Jack Chung <stevenjchung12@gmail.com>
2021-03-16 18:47:42 -0500

    Change form to stateful

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../actions';
 
-const BooksForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const BooksForm = ({ bookCreator }) => {
+  const [bookInfo, setBookInfo] = useState({
+    title: '',
+    category: '',
+  });
+
+  const handleChange = e => {
+    if (e.target.id === 'titleInput') {
+      setBookInfo({ ...bookInfo, title: e.target.value });
+    } else {
+      setBookInfo({ ...bookInfo, category: e.target.value });
+    }
+  };
+
+  const handleSubmit = () => {
+    if (bookInfo.title !== '' && bookInfo.category !== '') {
+      bookCreator(bookInfo);
+      setBookInfo({ title: '', category: '' });
+    } else {
+      setBookInfo({ ...bookInfo });
+    }
+  };
+
+  const categories = ['', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
   return (
     <>
       <form>
         <label>
           Book Title
-          <input type="text" />
+          <input onChange={handleChange} id="titleInput" type="text" value={bookInfo.title} />
         </label>
         <label>
           Category
-          <select>
+          <select id="categoryInput" onChange={handleChange} value={bookInfo.category}>
             {categories.map(cat => (
               <option key={cat} value={cat}>{cat}</option>
             ))}
           </select>
         </label>
-        <button type="submit">Add Book</button>
+        <button onClick={handleSubmit} type="button">Add Book</button>
       </form>
     </>
   );
 };
 
-export default BooksForm;
+BooksForm.propTypes = {
+  bookCreator: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  bookCreator: book => { dispatch(createBook(book)); },
+});
+
+export default connect(null, mapDispatchToProps)(BooksForm);
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,17 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
+export const randInt = (minim, maxim) => {
+  const min = Math.ceil(minim);
+  const max = Math.floor(maxim);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 export default (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return {
-        ...state, book: action.book,
-      };
+      return [
+        ...state, { id: randInt(1, 100), title: action.book.title, category: action.book.category },
+      ];
     case REMOVE_BOOK:
       return state.filter(book => book.id !== action.book.id);
     default:
