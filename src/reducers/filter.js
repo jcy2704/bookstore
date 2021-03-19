38fa574ebf3925c8982fa641a4314340b1374047
Steven Jack Chung <stevenjchung12@gmail.com>
2021-03-19 13:10:17 -0500

    Add CategoryFilter component, filter action, and finish filter reducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,10 @@
+import { CHANGE_FILTER } from '../actions';
+
+export default (state, action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+};
