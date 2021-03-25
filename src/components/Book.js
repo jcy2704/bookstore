3532d6c27beef02339daa69f9f4e426d40e0b79a
Steven J Chung <stevenjchung12@gmail.com>
2021-03-24 19:39:48 -0500

    Finish stlying book card

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,27 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = ({ book, delete: handleRemoveBook }) => (
-  <li className="book-card">
-    <div className="main-info">
-      <div>
-        <h4 className="book-cat">{book.category}</h4>
+  <li className="book-card flex align-c">
+    <div>
+      <div className="main-info">
+        <div>
+          <h4 className="book-cat">{book.category}</h4>
+        </div>
+        <div>
+          <h3 className="book-title">{book.title}</h3>
+        </div>
+        <div className="book-id">
+          Book ID:
+          {' '}
+          {book.id}
+        </div>
       </div>
-      <div>
-        <h3 className="book-title">{book.title}</h3>
-      </div>
-      <div className="book-id">
-        Book ID:
-        {' '}
-        {book.id}
+
+      <div className="lower-btns flex align-c">
+        <p>Comments</p>
+        <div className="btn-divider" />
+        <button className="rmv-btn" type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
+        <div className="btn-divider" />
+        <p>Edit</p>
       </div>
     </div>
 
-    <div className="lower-btns flex align-c">
-      <p>Comments</p>
-      <div className="btn-divider" />
-      <button className="rmv-btn" type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
-      <div className="btn-divider" />
-      <p>Edit</p>
+    <div className="progress flex align-c">
+      <div className="porcentage-oval flex align-c"><span /></div>
+      <div className="porcentage-cont">
+        <p className="porcentage">64%</p>
+        <p className="completed">Completed</p>
+      </div>
+    </div>
+
+    <div className="divider" />
+
+    <div className="chapter-cont">
+      <p className="current-ch">CURRENT CHAPTER</p>
+      <p className="chapter">Chapter 17</p>
+      <button className="updt-progress" type="button">UPDATE PROGRESS</button>
     </div>
   </li>
 );
